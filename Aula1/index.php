9650269e4ef29390b1f8c4a4c0cf04ded7019887
rafaelsilvaepsm <rafael.silva@epsm.pt>
2022-02-11 12:45:27 +0000

    Aula1

---
+++ Aula1/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>PHP - Exercício 1</title>
+    </head>
+    <body>
+
+    <?php 
+    
+    // Declarar variável
+    $nome = "Rafael Silva";
+
+    // Escrever string + variável
+    echo "O meu nome " . $nome;
+    ?>
+
+    </body>
+</html>
